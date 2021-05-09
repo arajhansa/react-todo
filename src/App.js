a3bc976173526c0c0a263bafc31393be2a0b6eb9
parakhRohan <31536085+parakhRohan@users.noreply.github.com>
2021-05-09 16:31:01 +0530

    Add item list elements

--- src/App.js
+++ src/App.js
@@ -8,8 +8,27 @@ function App() {
                     <input className="border-b-2 border-gray-300 p-2 focus:outline-none"/>
                     <button className="border-2 bg-red-300 p-2 focus:outline-none hover:bg-blue-300">Add</button>
                 </div>
-                <div>
-
+                <div className="w-4/5 m-auto mt-6">
+                    <ul className="space-y-6">
+                        <li className="text-gray-300 line-through">
+                            <div className="flex justify-between">
+                              <div className="flex items-center">
+                              <input type="checkbox"></input>
+                                <p className="ml-4">First Item</p>
+                              </div> 
+                              <button>Delete</button>  
+                            </div>
+                        </li>
+                        <li className="">
+                            <div className="flex justify-between">
+                              <div className="flex items-center">
+                              <input type="checkbox"></input>
+                                <p className="ml-4">Second Item</p>
+                              </div> 
+                              <button>Delete</button>  
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
