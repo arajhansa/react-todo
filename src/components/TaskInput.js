70544116f649091e47015c4d377f8dc6b9e55880
Bhavesh Salunke <Bhavesh.Salunke@nelkinda.com>
2021-05-09 22:14:54 +0530

    Add placeholder in task input

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -5,11 +5,12 @@ function TaskInput({addTask}) {
 
     const addNewTask = () => {
         addTask(inputRef.current.value)
+        inputRef.current.value = ""
     }
 
     return (
         <div className="flex justify-around p-2">
-            <input ref={inputRef} className="border-b-2 border-gray-300 p-2 focus:outline-none"/>
+            <input ref={inputRef} placeholder="Enter To-Do Task" className="border-b-2 border-gray-300 p-2 focus:outline-none"/>
             <button className="border-2 bg-red-300 p-2 focus:outline-none hover:bg-blue-300"
              onClick={addNewTask}>Add</button>
         </div>
