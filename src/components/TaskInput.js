ab9fa7a87c05f59b3e13d46b08b495a0491dead9
Bhavesh Salunke <Bhavesh.Salunke@nelkinda.com>
2021-05-09 22:54:30 +0530

    Add icon to add button

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -1,24 +1,28 @@
 import React, {useRef} from "react";
+import {PlusCircleIcon} from "@heroicons/react/outline";
 
 function TaskInput({addTask}) {
     const inputRef = useRef();
 
     const addNewTask = (event) => {
         if (event && event.key !== "Enter") return
-
+        if (inputRef.current.value === "") return
         addTask(inputRef.current.value)
         inputRef.current.value = ""
     }
 
 
     return (
-        <div className="flex justify-around p-2">
+        <div className="flex justify-around py-8 items-center">
             <input ref={inputRef} onKeyPress={(event => {
                 addNewTask(event)
             })} placeholder="Enter To-Do Task" className="border-b-2 border-gray-300 p-2 focus:outline-none"/>
-            <button className="border-2 bg-red-300 p-2 focus:outline-none hover:bg-blue-300"
+            {/*<button className="border-2 bg-red-300 p-2 focus:outline-none hover:bg-blue-300"
                     onClick={addNewTask}>Add
-            </button>
+            </button>*/}
+            <span onClick={() => addNewTask()} className="cursor-pointer">
+                    <PlusCircleIcon className="h-8 w-8 text-green-300"/>
+            </span>
         </div>
     );
 }
