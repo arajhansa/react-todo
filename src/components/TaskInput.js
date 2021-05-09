a9899268151c1e85562cb28e692a2f631dacf554
Bhavesh Salunke <Bhavesh.Salunke@nelkinda.com>
2021-05-09 22:31:00 +0530

    Add task on enter key press

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -3,16 +3,22 @@ import React, {useRef} from "react";
 function TaskInput({addTask}) {
     const inputRef = useRef();
 
-    const addNewTask = () => {
+    const addNewTask = (event) => {
+        if (event && event.key !== "Enter") return
+
         addTask(inputRef.current.value)
         inputRef.current.value = ""
     }
 
+
     return (
         <div className="flex justify-around p-2">
-            <input ref={inputRef} placeholder="Enter To-Do Task" className="border-b-2 border-gray-300 p-2 focus:outline-none"/>
+            <input ref={inputRef} onKeyPress={(event => {
+                addNewTask(event)
+            })} placeholder="Enter To-Do Task" className="border-b-2 border-gray-300 p-2 focus:outline-none"/>
             <button className="border-2 bg-red-300 p-2 focus:outline-none hover:bg-blue-300"
-             onClick={addNewTask}>Add</button>
+                    onClick={addNewTask}>Add
+            </button>
         </div>
     );
 }
