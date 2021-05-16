ea574d8c1d823810144633aa0b81509ef69f1724
Advay Rajhansa <rajhansa.advay@gmail.com>
2021-05-16 14:22:38 +0530

    Update view for task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -9,8 +9,8 @@ function TaskList() {
   const [list] = useContext(TaskContext)
 
   return (
-      <div className="w-4/5 m-auto mt-2 mb-6 flex overflow-hidden">
-        <div className="overflow-y-scroll pr-8 w-full">
+      <div className="w-5/6 ml-10 m-auto mt-2 mb-6 flex overflow-hidden">
+        <div className="overflow-y-scroll pr-4 w-full">
           <ul className="space-y-6 w-full">
             {list.map(element => <TaskListElement key={element.id} element={element}/>)}
           </ul>
